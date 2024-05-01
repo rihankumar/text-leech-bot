dbceaf163acff1d6d8b786457496e2a88ca3cfe0
Aman Pathak <155675919+Amanpathak1@users.noreply.github.com>
2024-05-01 13:21:39 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,6 +1,6 @@
 # SudoR2spr WOODcraft
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "1701393"
-api_hash = "96089a340f5892fd06aea683cbfb73c6"
-bot_token = "6872135659:AAwwm1jsMGNOFJnuICL_a1coI6CI6udAY6F4"
+api_id = "27184026"
+api_hash = "4c26e2cb6c6d9d57841b3c5ab6bb7605"
+bot_token = "6863049418:AAHru5Bk-_6fIUAskKxURQdinYt8y31usUg"
