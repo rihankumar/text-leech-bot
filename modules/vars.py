29f6a5b25786a34e64d68b4fcce51fbf72ffe998
Aman Pathak <155675919+Amanpathak1@users.noreply.github.com>
2024-05-02 13:20:43 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,4 +3,4 @@
 
 api_id = "27184026"
 api_hash = "4c26e2cb6c6d9d57841b3c5ab6bb7605"
-bot_token = "6863049418:AAHru5Bk-_6fIUAskKxURQdinYt8y31usUg"
+bot_token = "6343715160:AAGT8qVxxQjbGCwqvjDPHLLteduvRrqqh94"
