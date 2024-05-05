d1136778c3f31ea5201f415ea28ecf09d116ffde
STuHacker <132763286+rihankumar@users.noreply.github.com>
2024-05-05 11:54:35 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,4 +3,4 @@
 
 api_id = "27184026"
 api_hash = "4c26e2cb6c6d9d57841b3c5ab6bb7605"
-bot_token = "6343715160:AAGT8qVxxQjbGCwqvjDPHLLteduvRrqqh94"
+bot_token = "6499994285:AAFaSySS9Fc50k9-CKvOPlZ5-Fcv7ZH7b28"
